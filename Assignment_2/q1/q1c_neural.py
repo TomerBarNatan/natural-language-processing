32166def024e394772e224d21f245f4a52fadf7c
GiilDe <gilad.deutch@gmail.com>
2023-05-16 08:18:50 +0000

    q1

---
+++ Assignment_2/q1/q1c_neural.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+
+import numpy as np
+import random
+
+from softmax import softmax
+from sigmoid import sigmoid, sigmoid_grad
+from gradcheck import gradcheck_naive
+
+
+def forward(data, label, params, dimensions):
+    """
+    runs a forward pass and returns the probability of the correct word for eval.
+    label here is an integer for the index of the label.
+    This function is used for model evaluation.
+    """
+    # Unpack network parameters (do not modify)
+    ofs = 0
+    Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+
+    W1 = np.reshape(params[ofs:ofs + Dx * H], (Dx, H))
+    ofs += Dx * H
+    b1 = np.reshape(params[ofs:ofs + H], (1, H))
+    ofs += H
+    W2 = np.reshape(params[ofs:ofs + H * Dy], (H, Dy))
+    ofs += H * Dy
+    b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
+
+    # Compute the probability
+    data = data @ W1 + b1
+    data = sigmoid(data)
+    data = data @ W2 + b2
+    data = softmax(data)
+    return np.multiply(data, label).max(axis=1)
+
+
+
+def forward_backward_prop(data, labels, params, dimensions):
+    """
+    Forward and backward propagation for a two-layer sigmoidal network
+
+    Compute the forward propagation and for the cross entropy cost,
+    and backward propagation for the gradients for all parameters.
+
+    Arguments:
+    data -- M x Dx matrix, where each row is a training example.
+    labels -- M x Dy matrix, where each row is a one-hot vector.
+    params -- Model parameters, these are unpacked for you.
+    dimensions -- A tuple of input dimension, number of hidden units
+                  and output dimension
+    """
+
+    # Unpack network parameters (do not modify)
+    ofs = 0
+    Dx, H, Dy = (dimensions[0], dimensions[1], dimensions[2])
+
+    W1 = np.reshape(params[ofs:ofs+ Dx * H], (Dx, H))
+    ofs += Dx * H
+    b1 = np.reshape(params[ofs:ofs + H], (1, H))
+    ofs += H
+    W2 = np.reshape(params[ofs:ofs + H * Dy], (H, Dy))
+    ofs += H * Dy
+    b2 = np.reshape(params[ofs:ofs + Dy], (1, Dy))
+
+    ### YOUR CODE HERE: forward propagation
+    f1 = data @ W1 + b1
+    h = sigmoid(f1)
+    f2 = h @ W2 + b2
+    y_hat = softmax(f2)
+    ### YOUR CODE HERE: backward propagation
+    gradW2 = h.T @ (y_hat - labels)
+    gradb2 = (y_hat - labels).sum(axis=0)
+    gradW1 = data.T @ (np.multiply((y_hat - labels) @ W2.T, sigmoid_grad(h)))
+    gradb1 = (np.multiply((y_hat - labels) @ W2.T, sigmoid_grad(h))).sum(axis=0)
+    # Stack gradients (do not modify)
+    grad = np.concatenate((gradW1.flatten(), gradb1.flatten(),
+        gradW2.flatten(), gradb2.flatten()))
+
+    cost = (-1*np.log2(np.multiply(y_hat, labels).sum(axis=1))).sum()
+    return cost, grad
+
+
+def sanity_check():
+    """
+    Set up fake data and parameters for the neural network, and test using
+    gradcheck.
+    """
+    print("Running sanity check...")
+
+    N = 20
+    dimensions = [10, 5, 10]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in range(N):
+        labels[i, random.randint(0, dimensions[2]-1)] = 1
+
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+
+    gradcheck_naive(lambda params:
+                    forward_backward_prop(data, labels, params, dimensions), params)
+
+
+def your_sanity_checks():
+    """
+    Use this space add any additional sanity checks by running:
+        python q1c_neural.py
+    This function will not be called by the autograder, nor will
+    your additional tests be graded.
+    """
+    print("Running your sanity checks...")
+    N = 1
+    dimensions = [10, 5, 10]
+    data = np.random.randn(N, dimensions[0])   # each row will be a datum
+    labels = np.zeros((N, dimensions[2]))
+    for i in range(N):
+        labels[i, random.randint(0, dimensions[2]-1)] = 1
+    
+    params = np.random.randn((dimensions[0] + 1) * dimensions[1] + (
+        dimensions[1] + 1) * dimensions[2], )
+    res = forward(data, labels, params, dimensions)
+    assert res.shape == (N, ), "The shape of the result is wrong"
+    assert res.max() <= 1, "The result should be a probability"
+    print("Your forward sanity checks passed!")
+    
+    cost, grad = forward_backward_prop(data, labels, params, dimensions)
+    assert grad.shape == params.shape, "The shape of the gradient is wrong"
+    print("Your backward sanity checks passed!")
+
+
+if __name__ == "__main__":
+    sanity_check()
+    your_sanity_checks()
